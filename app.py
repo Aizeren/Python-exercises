6474d9ec4ce4d545b101958a20fa4352ce39856d
Baynova Ekaterina <ekaterinabainova@gmail.com>
2019-11-18 10:23:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,14 @@
-import numpy as np
-from flask import Flask, request
-
-def f(t, x, g=9.8, l=1):
-  theta, ang_vel  = x[0], x[1]
-  return np.array([ang_vel, -g*np.sin(theta)/l])
-
-def next_x(F, t, x, dt):
-  k1 = F(t, x)
-  k2 = F(t + dt/2, x + dt*k1/2)
-  k3 = F(t + dt/2, x + dt*k2/2)
-  k4 = F(t + dt, x + dt*k3)
-  return x + (dt/6)*(k1 + 2*k2 + 2*k3 + k4)
-  
-def rk4(F, t0, x0, t, dt=0.1):
-  t_array = np.arange(t0, t+dt, dt)
-  t_array[-1] = t
-
-  x_array = np.zeros((t_array.size, x0.size), dtype = float)
-  x_array[0] = x0
-
-  for i in range(1, t_array.size - 1):
-    x_array[i] = next_x(F, t_array[i-1], x_array[i-1], dt)
-
-  x_array[-1] = next_x(F, t_array[-2], x_array[-2], t_array[-1] - t_array[-2])
-  return t_array, x_array
-
+from flask import Flask, render_template
+import subprocess
 app = Flask(__name__)
 
-@app.route("/test", methods=['GET', 'POST'])
-def index(dt="0"):
-    result = ""
-    result_t, result_x = rk4(f, 0, np.array([np.pi/4, 0]), 100, dt=float(dt))
-    return str(result_x)
 @app.route("/")
 def main():
-    return "hello"
-  
-if __name__ == '__main__':
-    app.run()
+	return render_template('index.html')
+@app.route('/process_data/', methods=['POST'])
+def script():
+	return "Hello"    	
+	#subprocess.run("sh ./script.sh", shell = True)
+
+if __name__ == "__main__":
+	app.run()
